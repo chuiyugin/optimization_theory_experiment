fba9582720eb41fa8ccdc0fab94e5d55d6b15842
chuiyugin <1726123207@qq.com>
2024-10-23 22:28:24 +0800

    update

--- 实验四/iteration_method_2.m
+++ 实验四/iteration_method_2.m
@@ -8,13 +8,14 @@ grad_mf = @(x,y) [-400*x.*(y - x.^2) - 2*(1 - x); 200*(y - x.^2)];
 hesse_mf = @(x,y) [-400*y + 1200*x.^2 + 2, -400*x ; -400*x , 200];
 
 % 参数设置
-x0 = [5; 5]; % 初始点
+x0 = [0; 0]; % 初始点
 maxIter = 1000; % 最大迭代次数
-tolerance = 1e-2; % 精度
+tolerance = 1e-3; % 精度
 
 % 初始化
 x = x0;
 %迭代计算
+tic; % 开始计时
 for iter = 1:maxIter
     % 计算梯度矩阵
     gradient = grad_mf(x(1), x(2));
@@ -30,3 +31,5 @@ for iter = 1:maxIter
     % 输出最终结果
     fprintf('牛顿法第%d次迭代后: x=%.4f, y=%.4f, f(x,y)=%.4f\n', iter,x(1), x(2), mf(x(1), x(2)));
 end
+tElapsed = toc; % 结束计时并获取经过的时间
+fprintf('程序执行时间：%.5f秒\n', tElapsed);
